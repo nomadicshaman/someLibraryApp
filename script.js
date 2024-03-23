948a93b70925830d741b1e087e92e2389e899dad
Burak Bozan <burakbozantr@gmail.com>
2024-03-23 21:38:38 +0200

    add adding book function to the array

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+let bookInput = document.querySelector('.inputBox');
+let addButton = document.querySelector('.addButton');
+
+
+
+
 const myLibrary = [];
 
-function Book {
+function Book() {
 
 }
 
-function addBookToLibrary {
+
+
+function addBookToLibrary() {
+    myLibrary.push(bookInput.value);
+    bookInput.value ='';
+    console.log(myLibrary);
     
 }
+
+addButton.addEventListener('click', addBookToLibrary);
+
+
+
+
+bookInput.addEventListener("keypress", function(event) {
+
+    if (event.key === "Enter") {
+
+      event.preventDefault();
+
+      addButton.click();
+    }
+  }); 
